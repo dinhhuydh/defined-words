078a895cd948f55689aa0bb3d638f37083c17641
huyd <dinhhuydh@gmail.com>
2016-02-12 18:50:23 +0700

    Remove debug and empty line

--- DefinedWords/ViewController.swift
+++ DefinedWords/ViewController.swift
@@ -16,14 +16,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         wordManager.loadWords()
-        print("\(wordManager.words)")
-
-        wordList.reloadData()
     }
     
     //returning to view
     override func viewWillAppear(animated: Bool) {
-        print("\(wordManager.words.count)")
         wordList.reloadData()
     }
 
@@ -36,19 +32,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         
     }
-
-    
     
     //UITableViewDataSource
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print("\(wordManager.words.count)")
         return wordManager.words.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
         let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier:  "test")
-        print("\(wordManager.words)")
-        print("\(wordManager.words[indexPath.row])")
         cell.textLabel?.text = wordManager.words[indexPath.row]
         
         return cell
